431f348a4b78f646f3a4a958f33144be75c2d685
akaunghtet045-driod <akaunghtet045@gmail.com>
2026-07-02 14:27:18 +0630

    new add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/Hero.css';
 import myimage from '../assets/myimage.jpg'
+import mycv from '../assets/cv.pdf'
 
 function Hero() {
   return (
@@ -8,24 +9,24 @@ function Hero() {
       <div className="hero-container">
         <div className="hero-left">
           <h3>Hello, I'm</h3>
-          <h1>Kaung Htet Aung</h1>
+          <h1>Kyaw Yi Mon</h1>
           <h2>Frontend Developer / React Developer</h2>
           <p className="hero-desc">
             I craft clean, high-performance, and pixel-perfect digital user experiences. Focused on turning complex layout concepts into highly functional modern web designs.
           </p>
           <div className="hero-btns">
-            {/* NOTE: Replace Resume PDF Link here for Download CV */}
-            <a href="file:///C:/Users/ACER/Downloads/Telegram%20Desktop/Kyaw_Yi_Mon_Frontend_Developer_CV.pdf%E2%81%A0.pdf" className="btn-primary">Download CV</a>
+            
+            <a href={mycv} download='Kyaw_yi_mon_Frontend_Developer_CV.pdf' className="btn-primary">Download CV</a>
             <a href="#projects" className="btn-secondary">View Projects</a>
           </div>
         </div>
         <div className="hero-right">
           <div className="image-wrapper">
             <div className="glow-effect"></div>
-            {/* NOTE: Replace profile image placeholder source here */}
+            
             <img 
               src={myimage} 
-              alt="Wai Lynn Naing Profile" 
+              alt="kyaw yi mon Profile" 
               className="hero-img" 
             />
           </div>
